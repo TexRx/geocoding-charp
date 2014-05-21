d13ee47e29c7d35ff2c4326d9fc6ea3f377b3389
askesian <cwigley@onenorth.com>
2014-05-20 21:13:49 -0500

    Remove hard ref to Json.NET and replace with Nuget package reference

---
+++ src/GeocodingApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿/**
+ * See SharedAssemblyInfo.cs for generic assembly information
+ * that applies to all assemblies in this solution
+ */
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GeocodingApi")]
+[assembly: AssemblyDescription("C# Wrapper of the Google Maps geocoding service")]
+
+// This is the version you would use when talking to customers or for display
+[assembly: AssemblyInformationalVersion("Version 1 - Initial Release")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fbb0c622-fe79-45a2-89ed-f1dc5081eaaa")]
---
+++ src/GeocodingApiDemoConsoleApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿/**
+ * See SharedAssemblyInfo.cs for generic assembly information
+ * that applies to all assemblies in this solution
+ */
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GeocodingApiDemoConsoleApp")]
+[assembly: AssemblyDescription("Demo Console App for Geocoding API")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("97d1d9f1-705b-41ad-be0c-30daa26b39f0")]
